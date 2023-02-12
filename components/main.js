2582b0fe0f0f74865d69c0a23d5d28730cdfff85
Blackxok <nasridoni1@gmail.com>
2023-02-13 01:44:10 +0500

    chng

--- components/main.js
+++ components/main.js
@@ -1,40 +1,26 @@
-const btns = document.querySelector('#button_s');
+const todo = document.querySelector('#todo');
 const input = document.querySelector('.form_input');
 const add_btn = document.querySelector('.add_b');
 const del_btn = document.querySelector('.clear_b');
 const list = document.querySelector('.top_l');
 
-// submit uchun holat
-btns.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const in_text = input.value;
-    list.innerHTML += `<div class="list">
-        <div class="item">${in_text}</div>
-        <i class="fa-solid fa-check"></i>
-        <i class="fa-solid fa-xmark"></i>
-    </div>`
-
-    input.value = '';
-});
-
-// delet uchun holat
-del_btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    input.value = '';
-    console.log("del_btn");
-});
-
-// enter uchun holat
-input.addEventListener('keyup', function (e) {
-    e.preventDefault();
-    const in_text = input.value;
-    if (e.keyCode === 13) {
-        list.innerHTML += `<div class="list">
-        <div class="item">${in_text}</div>
-        <i class="fa-solid fa-check"></i>
-        <i class="fa-solid fa-xmark"></i>
-    </div>`
-
-    input.value = '';
-    }
+// multiple events
+['submit', 'keyup', 'click'].forEach(function (events) {
+    todo.addEventListener(events, function (e) {
+        e.preventDefault();
+        const in_text = input.value;
+        if (e.keyCode === 13 || e.target.className === 'add_b') {
+            list.innerHTML += `<div class="list">
+                <div class="item">${in_text}</div>
+                <i class="fa-solid fa-check"></i>
+                <i class="fa-solid fa-xmark"></i>
+                </div>`
+            input.value = '';
+            input.focus();
+        } else if (e.target.className === 'clear_b') {
+            input.value = input.value.toString().slice(0, -1);
+        } else {
+            return;
+        };
+    });
 });

24ddac5b25166204028d40737d8be1213cd81e25
Blackxok <nasridoni1@gmail.com>
2023-02-16 23:08:11 +0500

    start cng

--- components/main.js
+++ components/main.js
@@ -3,11 +3,34 @@ const input = document.querySelector('.form_input');
 const add_btn = document.querySelector('.add_b');
 const del_btn = document.querySelector('.clear_b');
 const list = document.querySelector('.top_l');
+const ico = document.querySelector('#ico');
+const lines_icon = document.querySelector('.lines');
+const file_icon = document.querySelector('.file');
+
 
 // multiple events
 ['submit', 'keyup', 'click'].forEach(function (events) {
     todo.addEventListener(events, function (e) {
         e.preventDefault();
+
+        // list length ---------------------------------------
+
+        if (list.children.length > 1) {
+            console.log("katta");
+            lines_icon.style.display = "block";
+            file_icon.style.display = "none";
+        } else {
+            console.log("mayda");
+        }
+        console.log("working");
+        console.log(ico);
+        console.log(list.children.length);
+
+
+        // -------------------------------------------
+
+
+
         const in_text = input.value;
         if (e.keyCode === 13 || e.target.className === 'add_b') {
             list.innerHTML += `<div class="list">
@@ -24,3 +47,10 @@ const list = document.querySelector('.top_l');
         };
     });
 });
+
+// empty or full list
+
+
+
+
+
